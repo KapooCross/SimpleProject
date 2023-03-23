69285381aea44cf72cc83ee6992db5d14d86db21
KapooCross <kapoocross@gmail.com>
2023-03-23 13:56:38 -0700

    new project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //commit
+        //push
+        //controller - главный
+        System.out.println("Hello world!");
+    }
+}
